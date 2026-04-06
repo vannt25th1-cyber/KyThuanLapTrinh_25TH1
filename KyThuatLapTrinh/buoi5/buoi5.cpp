508b784f28ee8e27d643a67ddf892998d4e5bc89
Administrator <Administrator@PMT2030>
2026-04-06 14:09:13 +0700

    buoi5

--- KyThuatLapTrinh/buoi5/buoi5.cpp
+++ KyThuatLapTrinh/buoi5/buoi5.cpp
@@ -5,126 +5,162 @@
 using namespace std;
 
 struct Author {
-    int id;
-    string name;
-    friend istream& operator >> (istream& in, Author& a) {
-        cout << "Author information: " << endl;
-        cout << "\t+ Id: ";
-        in >> a.id;
-        cout << "\t+ Name: ";
-        in.ignore();
-        getline(in, a.name);
-        return in;
-    }
+	int id;
+	string name;
+	friend istream& operator >> (istream& in, Author& a) {
+		cout << "Author information: " << endl;
+		cout << "\t+ Id: ";
+		in >> a.id;
+		cout << "\t+ Name: ";
+		in.ignore();
+		getline(in, a.name);
+		return in;
+	}
 };
 
 struct Book {
-    int id;
-    string name;
-    Author author;
-    friend ostream& operator << (ostream& os, const Book& b) {
-        os << "Book information: " << endl;
-        os << "\t+ Id:" << b.id << endl;
-        os << "\t+ Name:" << b.name << endl;
-        os << "\t+ Author name:" << b.author.name << endl;
-        return os;
-    }
-    friend istream& operator << (ostream& os, const Book& b) {
-        cout << "Book information: " << endl;
-        cout << "\t+ Id:";
-        in >> b.id;
-        cout << "\t+ Name:";
-        in.ignore();
-        getLine(in, b.name);
-        in >> b.author;
-        return in;
-    };
-
-    struct Node {
-        Book data;
-        Node* next;
-        void Create(Book b) {
-            data = b;
-            next = nullptr;
-        }
-    };
-
-    struct LinkedList {
-        Node* head;
-    };
-    void Show(LinkedList books) {
-        if (books.head == NULL) {
-            cout << "No book available" << endl;
-            return;
-        }
-        Node* item = books.head;
-        while (item != NULL) {
-            cout << item->data;
-            item = item->next;
-        }
-    }
-    void AddFirst(Node* p) {
-        p->next = head;
-        head = p;
-    }
+	int id;
+	string name;
+	Author author;
+	friend ostream& operator<< (ostream& os, const Book& b) {
+		os << "Book information: " << endl;
+		os << "\t+ Id:" << b.id << endl;
+		os << "\t+ Name:" << b.name << endl;
+		os << "\t+ Author name:" << b.author.name << endl;
+		return os;
+	}
+	friend istream& operator>> (istream& in, Book& b) {
+		cout << "Book information: " << endl;
+		cout << "\t+ Id:";
+		in >> b.id;
+		cout << "\t+ Name:";
+		in.ignore();
+		getline(in, b.name);
+		in >> b.author;
+		return in;
+	}
 };
 
+struct Node {
+	Book data;
+	Node* next;
+	Node* Create(Book b) {
+		Node* result = new Node;
+		result->data = b;
+		result->next = nullptr;
+		return result;
+
+	}
+};
+
+struct LinkedList {
+	Node* head;
+	void Show() {
+		if (head == NULL) {
+			cout << "No book available" << endl;
+			return;
+		}
+		Node* item = head;
+		while (item != NULL) {
+			cout << item->data;
+			item = item->next;
+		}
+	}
+	void AddFirst(Node* p) {
+		p->next = head;
+		head = p;
+	}
+	bool Remove(int removeId) {
+		if (head == NULL) {
+			cout << "No book available" << endl;
+			return true;
+
+		}
+		Node* item = head;
+		if (item->data.id == removeId) { //Xoa dau danh sach
+			head = item->next;
+			delete item;
+			return true;
+
+		}
+		while (item->next != NULL) {
+			if (item->data.id == removeId) {
+				Node* temp = item->next;
+				item->next = item->next->next;
+				delete temp;
+				return true;
+
+			}
+			item = item->next;
+		}
+		return false;
+	}
+};
 int main()
 {
-    LinkedList books = { NULL };
-    do {
-        system("cls");
-        cout << "---------BOOK MANAGEMENT-------" << endl;
-        cout << "1. show all books " << endl;
-        cout << "2. Add a book " << endl;
-        cout << "3.Delete a book " << endl;
-        cout << "4. Update a book " << endl;
-        cout << "5. Find book " << endl;
-        cout << "6. Export to file" << endl;
-        cout << "7. Import from file" << endl;
-        cout << "0. Exit" << endl;
-        cout << "-----------------------------" << endl;
-        cout << "Enter your choice: ";
-        int choice;
-        cin >> choice;
-        switch (choice)
-        {
-        case 1: {
-            books.Show();
-            break;
-        }
-        case 2: {
-            Book b;
-            cin >> b;
-            Node* newNode = new Node;
-            newNode->Create(b);
-            books.AddFirst(newNode);
-            break;
-        }
-        case 3: {
-            break;
-        }
-        case 4: {
-            break;
-        }
-        case 5: {
-            break;
-        }
-        case 6: {
-            break;
-        }
-        case 7: {
-            break;
-        }
-        case 0: {
-            break;
-        }
-        default: {
-            cout << "Inalid choice, try again" << endl;
-            break;
-        }
+	LinkedList books = { NULL };
+	do {
+		system("cls");
+		cout << "---------BOOK MANAGEMENT-------" << endl;
+		cout << "1. show all books " << endl;
+		cout << "2. Add a book " << endl;
+		cout << "3.Delete a book " << endl;
+		cout << "4. Update a book " << endl;
+		cout << "5. Find book " << endl;
+		cout << "6. Export to file" << endl;
+		cout << "7. Import from file" << endl;
+		cout << "0. Exit" << endl;
+		cout << "-----------------------------" << endl;
+		cout << "Enter your choice: ";
+		int choice;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1: {
+			books.Show();
+			break;
+		}
+		case 2: {
+			Book b;
+			cin >> b;
+			Node* newNode = new Node;
+			newNode->Create(b);
+			books.AddFirst(newNode);
+			break;
+		}
+		case 3: {
+			int removeId;
+			cout << "Enter book's id to remove:";
+			cin >> removeId;
+			bool res = books.Remove(removeId);
+			if (res)
+				cout << "Remove book successfully" << endl;
+			else
+				cout << "Invalid book id" << endl;
 
-        }
-    } while (true);
+
+			break;
+		}
+		case 4: {
+			break;
+		}
+		case 5: {
+			break;
+		}
+		case 6: {
+			break;
+		}
+		case 7: {
+			break;
+		}
+		case 0: {
+			break;
+		}
+		default: {
+			cout << "Inalid choice, try again" << endl;
+			break;
+		}
+		}
+	} while (true);
 }
 
